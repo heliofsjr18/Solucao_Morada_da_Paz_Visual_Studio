c31585af5eb6ae8fb1877c9529676f754314bc90
heliofsjr18 <heliofsjr18@hotmail.com>
2016-10-29 11:42:07 -0300

    Add Crud Especializacao

--- Morada_da_paz_Biblioteca/DadosAcesso/CrudEspecializacaoUsuarioImplements.cs
+++ Morada_da_paz_Biblioteca/DadosAcesso/CrudEspecializacaoUsuarioImplements.cs
@@ -15,28 +15,115 @@ namespace Morada_da_paz_Biblioteca.DadosAcesso
     {
         public void inserir(especializacao_usuario eu)
         {
-            throw new NotImplementedException();
+            try
+            {
+                SqlConnection conexao = conectar();
+                string insertSql = "INSERT INTO especializacao_usuario (descricao)";
+                insertSql += " values ";
+                insertSql += " (@descricao)";
+
+                SqlCommand comand = new SqlCommand(insertSql, conexao);
+                comand.Parameters.AddWithValue("@descricao", eu.Descricao);
+                comand.ExecuteNonQuery();
+                comand.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public void atualizar(especializacao_usuario eu)
         {
-            throw new NotImplementedException();
+            try
+            {
+                SqlConnection conexao = conectar();
+                string updateSql = "UPDATE especializacao_usuario SET descricao = @descricao WHERE id = @id)";
+
+                SqlCommand comand = new SqlCommand(updateSql, conexao);
+                comand.Parameters.AddWithValue("@id", eu.Id);
+                comand.Parameters.AddWithValue("@descricao", eu.Descricao);
+                comand.ExecuteNonQuery();
+                comand.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public especializacao_usuario consultar(especializacao_usuario eu)
         {
-            throw new NotImplementedException();
+            try
+            {
+                SqlConnection conexao = conectar();
+                string querySql = "SELECT id, descricao FROM especializacao_usuario WHERE id = @idpar";
+
+                SqlCommand comand = new SqlCommand(querySql, conexao);
+                comand.Parameters.AddWithValue("@idpar", eu.Id);
+                especializacao_usuario euConsulta = new especializacao_usuario();
+
+                SqlDataReader reader = comand.ExecuteReader();
+
+                euConsulta.Id = reader.GetInt32(reader.GetOrdinal("id"));
+                euConsulta.Descricao = reader.GetString(reader.GetOrdinal("descricao"));
+
+                return euConsulta;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public void deletar(especializacao_usuario eu)
         {
-            throw new NotImplementedException();
+            try
+            {
+                SqlConnection conexao = conectar();
+                string deleteSql = "DELETE FROM especializacao_usuario WHERE id = @idpar";
+
+                SqlCommand comand = new SqlCommand(deleteSql, conexao);
+                comand.Parameters.AddWithValue("@idpar", eu.Id);
+
+                comand.ExecuteNonQuery();
+                comand.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
 
         public List<especializacao_usuario> listar()
         {
-            throw new NotImplementedException();
+            try
+            {
+                SqlConnection conexao = conectar();
+                string querySql = "SELECT id, descricao FROM especializacao_usuario";
+
+                SqlCommand comand = new SqlCommand(querySql, conexao);
+
+                SqlDataReader reader = comand.ExecuteReader();
+
+                List<especializacao_usuario> lista = new List<especializacao_usuario>();
+                while (reader.NextResult())
+                {
+                    especializacao_usuario euConsulta = new especializacao_usuario();
+
+                    euConsulta.Id = reader.GetInt32(reader.GetOrdinal("id"));
+                    euConsulta.Descricao = reader.GetString(reader.GetOrdinal("descricao"));
+
+                    lista.Add(euConsulta);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
